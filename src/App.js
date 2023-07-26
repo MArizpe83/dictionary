db5f020a5e3cd4bd64a1ebf0940921aa2b41e091
MArizpe83 <124353870+MArizpe83@users.noreply.github.com>
2023-07-26 20:00:21 +0200

    Fixed Front Page
    Dictionary did not accept Petricor as default.
    
    Added links to socials.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <main>
-        <Dictionary />
-      </main>
-      <footer className="Footer">
-        <p>
+      <div className="container">
+        <Dictionary defaultKeyword="cat" />
+        <footer>
+          This project was coded by{" "}
           <a
-            href="https://github.com/MArizpe83"
+            href="https://www.linkedin.com/in/mildred-arizpe-vicencio/"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
-            Open-source code{" "}
+            Mildred Arizpe
           </a>{" "}
-          by Mildred Arizpe
-        </p>
-      </footer>
+          and is{" "}
+          <a
+            href="https://github.com/MArizpe83/dictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://lustrous-mochi-ab690c.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
+
+export default App;
