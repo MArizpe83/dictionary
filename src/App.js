37df003a3248fca96549d24d262c6e6da372f111
MArizpe83 <124353870+MArizpe83@users.noreply.github.com>
2023-07-31 19:10:43 +0200

    Changes to CSS, Added Header

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+import header from "./header.png";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Dictionary defaultKeyword="Book" />
+        <header className="App-header">
+          <img src={header} className="header" alt="App Header" />
+        </header>
+        <Dictionary defaultKeyword="Library" />
         <footer>
-          This project was coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/mildred-arizpe-vicencio/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Mildred Arizpe
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/MArizpe83/dictionary"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://lustrous-mochi-ab690c.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-          </a>
+          <p>
+            {" "}
+            This project was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/mildred-arizpe-vicencio/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Mildred Arizpe
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/MArizpe83/dictionary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://lustrous-mochi-ab690c.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </p>
         </footer>
       </div>
     </div>
